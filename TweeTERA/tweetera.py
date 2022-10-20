a5b840b93ff52493681a688daae52cbb0524fda5
BrianS <brianseko@gmail.com>
2022-10-19 23:35:20 -0400

    removing itertools

--- TweeTERA/tweetera.py
+++ TweeTERA/tweetera.py
@@ -27,7 +27,6 @@ def install_dependencies():
     subprocess.check_call([sys.executable, '-m', 'pip', 'install','DateTime'])
     subprocess.check_call([sys.executable, '-m', 'pip', 'install','altair-saver'])
     subprocess.check_call([sys.executable, '-m', 'pip', 'install','collection'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install','itertools'])
 
     import nltk
     nltk.download('omw-1.4')
